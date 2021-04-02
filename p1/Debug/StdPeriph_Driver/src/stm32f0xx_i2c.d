05daaa8891abde2c36964ae6dd4e915c60a12cc2
Zicong Wang <actonnwang@gmail.com>
2021-04-01 22:46:02 -0400

    Add files via upload

---
+++ p1/Debug/StdPeriph_Driver/src/stm32f0xx_i2c.d
@@ -0,0 +1,86 @@
+StdPeriph_Driver/src/stm32f0xx_i2c.o: \
+ ../StdPeriph_Driver/src/stm32f0xx_i2c.c \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_i2c.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/device/stm32f0xx.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/core/core_cm0.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/core/core_cmInstr.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/core/core_cmFunc.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/device/system_stm32f0xx.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_conf.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_cec.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_crc.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_comp.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_dac.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_dbgmcu.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_dma.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_exti.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_flash.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_gpio.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_syscfg.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_i2c.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_iwdg.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_pwr.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_rcc.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_rtc.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_spi.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_tim.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_usart.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_wwdg.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_misc.h \
+ C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_rcc.h
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_i2c.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/device/stm32f0xx.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/core/core_cm0.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/core/core_cmInstr.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/core/core_cmFunc.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/CMSIS/device/system_stm32f0xx.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_conf.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_cec.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_crc.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_comp.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_dac.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_dbgmcu.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_dma.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_exti.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_flash.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_gpio.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_syscfg.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_i2c.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_iwdg.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_pwr.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_rcc.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_rtc.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_spi.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_tim.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_usart.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_wwdg.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_misc.h:
+
+C:/Users/acton/OneDrive/Windows/Desktop/Spring2021/ECE\ 477/p1/StdPeriph_Driver/inc/stm32f0xx_rcc.h:
